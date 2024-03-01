89fd3d529a6810b9aba0b8bc1d9af42d474506dd
blik <blik@mail.bliklabs.com>
2024-02-29 22:29:59 -0600

    initial commit. skeleton for geft.

---
+++ load_environment.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+PATH='/home/b/.local/bin:/usr/bin:/usr/sbin'
+
+PROJECT_OWNER="$(whoami)"
+PROJECT_PATH="${PWD}"
+PROJECT_END_STATE="${PROJECT_PATH}/hosts"
+ANSIBLE_INVENTORY="${PROJECT_END_STATE}/hosts.ini"
+ANSIBLE_PRIVATE_KEY_FILE="${PROJECT_PATH}/.secret"
+ANSIBLE_SSH_ARGS="-o StrictHostKeyChecking=no -o ForwardAgent=yes"
+ANSIBLE_ROLES_PATH="${PROJECT_PATH}/roles"
+ANSIBLE_GALAXY_CACHE_DIR="${PROJECT_PATH}/var"
+ANSIBLE_LOG_PATH="${PROJECT_PATH}/var/ansible.log"
+ANSIBLE_PYTHON_INTERPRETER='/usr/bin/python3'
+ANSIBLE_PLAYBOOK_DIR="${PROJECT_PATH}/playbooks"
+
+eval $(ssh-agent -s)
+ssh-add "${ANSIBLE_PRIVATE_KEY_FILE}"
