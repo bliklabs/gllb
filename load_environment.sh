f4488c59d7098474954ac6f5be6085d52a0b3f49
blik <blik@mail.bliklabs.com>
2024-03-16 02:05:08 -0500

    cleanup load_env

--- load_environment.sh
+++ load_environment.sh
@@ -5,14 +5,11 @@ PROJECT_PATH="${PWD}"
 PROJECT_END_STATE="${PROJECT_PATH}/hosts"
 ANSIBLE_INVENTORY="${PROJECT_END_STATE}/hosts.ini"
 ANSIBLE_PRIVATE_KEY_FILE="${PROJECT_PATH}/.secret"
-ANSIBLE_SSH_ARGS="-o StrictHostKeyChecking=no -o ForwardAgent=yes"
-ANSIBLE_ROLES_PATH="${PROJECT_PATH}/roles"
+ANSIBLE_SSH_ARGS="-o StrictHostKeyChecking=no -o ForwardAgent=yes -o UserKnownHostsFile=/dev/null"
 ANSIBLE_GALAXY_CACHE_DIR="${PROJECT_PATH}/var"
 ANSIBLE_LOG_PATH="${PROJECT_PATH}/var/ansible.log"
-ANSIBLE_PYTHON_INTERPRETER='/usr/bin/python3'
+ANSIBLE_PYTHON_INTERPRETER='/usr/bin/env python3'
 ANSIBLE_PLAYBOOK_DIR="${PROJECT_PATH}/playbooks"
-ANSIBLE_CACHE_PLUGIN="jsonfile"
-ANSIBLE_CACHE_PLUGIN_CONNECTION="${PROJECT_PATH}/cache/"
 ANSIBLE_SSH_USER="root"
 
 eval $(ssh-agent -s)
